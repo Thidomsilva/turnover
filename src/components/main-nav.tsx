b39021489e5d14b3e4f14b3bc57487c149b2572e
Thiago Domingos <thiago@sagacy.com.br>
2025-07-31 19:26:30 +0000

    botao de voltar qndo entrar em configuracoes, perfil, etc

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,22 +1,39 @@
+
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
   return (
     <nav
       className={cn('flex items-center space-x-4 lg:space-x-6', className)}
       {...props}
     >
-      <Link href="/dashboard" className="flex items-center gap-2">
-        <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold md:inline-block">
-          Gestão de Turnover
-        </span>
-      </Link>
+      {pathname !== '/dashboard' ? (
+        <Button asChild variant="ghost">
+          <Link href="/dashboard" className="flex items-center gap-2">
+            <ArrowLeft className="h-4 w-4" />
+            <span>Voltar</span>
+          </Link>
+        </Button>
+      ) : (
+        <Link href="/dashboard" className="flex items-center gap-2">
+          <Icons.logo className="h-6 w-6" />
+          <span className="hidden font-bold md:inline-block">
+            Gestão de Turnover
+          </span>
+        </Link>
+      )}
     </nav>
   );
 }
